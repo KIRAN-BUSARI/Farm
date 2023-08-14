7e2b9ac5c1affebde843b32c9d0645bc61f63dfa
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-08-14 15:41:08 +0530

    Adding file and deleted the unused files

--- Auth/Back_End/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const PORT = process.env.PORT || 5004;
-
-const app = require("./app");
-
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
