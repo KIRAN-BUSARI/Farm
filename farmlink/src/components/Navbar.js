c367df39bf541b523f600903ad02048d6a2623e5
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-08-21 19:36:04 +0530

    Adding the Tailwind css

--- farmlink/src/components/Navbar.js
+++ farmlink/src/components/Navbar.js
@@ -7,13 +7,13 @@ export default function Navbar(props) {
             <div className="nav-container">
                 {/* <video autoPlay loop muted playsInline className='bgv' src={require('../Images/bgv.mp4')} typeof='video/mp4'></video> */}
                 <nav>
-                    <h2 className='heading'>FarmLink.!</h2>
+                    <h2 className='text-xl font-sans font-bold text-[#fff]'>FarmLink.!</h2>
                     <ul>
                         <li><a href="/about">ABOUT</a></li>
                         <li><a href="/communities">COMMUNITIES</a></li>
                         <li><a href="/support">SUPPORT</a></li>
                         <li><a href="/store">STORE</a></li>
-                        <li><button>Signin</button></li>
+                        <li><button className='bg-[#fff] px-[6px] py-[3px]'>Signin</button></li>
                     </ul>
                 </nav>
             </div>
